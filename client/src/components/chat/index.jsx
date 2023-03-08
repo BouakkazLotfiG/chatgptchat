780a403f05bd7c185bffbbda8346e95c2081370c
Lotfi <bouakkaz144.lotfi@gmail.com>
2023-03-08 12:13:50 +0100

    Header component with hard coded data lotfi

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import {
+  useMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow,
+} from 'react-chat-engine-advanced';
+import Header from '@/components/Header';
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    'lotfi',
+    'lotfi'
+  );
+
+  return (
+    <div style={{ flexBasis: '100%' }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{ height: '100vh' }}
+        renderChatHeader={(chat) => <Header chat={chat} />}
+      />
+    </div>
+  );
 };
 
 export default Chat;
