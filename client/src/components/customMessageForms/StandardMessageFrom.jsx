46fdcb8222cd2d936cfaafc7fcb3bc43f2a71c69
Lotfi <bouakkaz144.lotfi@gmail.com>
2023-03-08 13:01:41 +0100

    dropzone, clipping image

--- client/src/components/customMessageForms/StandardMessageFrom.jsx
+++ client/src/components/customMessageForms/StandardMessageFrom.jsx
@@ -1,10 +1,13 @@
-import { XMarkIcon } from '@heroicons/react/24/solid';
+import { PaperClipIcon, XMarkIcon } from '@heroicons/react/24/solid';
 import React, { useState } from 'react';
+import Dropzone from 'react-dropzone';
 
 const StandardMessageFrom = () => {
   const [message, setMessage] = useState('');
   const [attachment, setAttachment] = useState('');
   const [preview, setPreview] = useState('');
+
+  const handleChange = (e) => setMessage(e.target.value);
 
   return (
     <div className='message-form-container'>
@@ -25,7 +28,38 @@ const StandardMessageFrom = () => {
           />
         </div>
       )}
-      <div>nigga</div>
+      <div className='message-form'>
+        <div className='message-form-input-container'>
+          <input
+            type='text'
+            className='message-form-input'
+            value={message}
+            onChange={handleChange}
+            placeholder='Send Message ...'
+          />
+        </div>
+        <div className='message-form-icons'>
+          <Dropzone
+            acceptedFiles='.jpg, .jpeg, .png'
+            multiple={false}
+            noClick={true}
+            onDrop={(acceptedFiles) => {
+              setAttachment(acceptedFiles[0]);
+              setPreview(URL.createObjectURL(acceptedFiles[0]));
+            }}
+          >
+            {({ getRootProps, getInputProps, open }) => (
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <PaperClipIcon
+                  className='message-form-icon-clip'
+                  onClick={open}
+                />
+              </div>
+            )}
+          </Dropzone>
+        </div>
+      </div>
     </div>
   );
 };
